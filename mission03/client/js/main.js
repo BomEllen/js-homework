011f9f8b79c90eae2d2069ba85fda6c7cb4f3aaa
BomEllen <jjjj200100@gmail.com>
2024-11-24 00:27:09 +0900

    코드 구조 수정 - 변경하는 요소들 함수화
    디자인 수정
    - nickName폰트 지정

--- mission03/client/js/main.js
+++ mission03/client/js/main.js
@@ -6,37 +6,49 @@ import { data } from './data.js';
 
 
 const buttons = document.querySelectorAll(".nav button");
-const body = document.querySelector("body");
+const bodyElement = document.querySelector("body");
 const visualImg = document.querySelector(".visual img");
 const nickName = document.querySelector(".nickName");
 
 
+// 바디 배경 색 변경 함수
+function setBgColor(color) {
+  bodyElement.style.background = `linear-gradient(to bottom, ${color[0]}, ${color[1]})`;
+}
 
+
+// 이미지 정보 변경 함수
+function setImg(element, name, alt) {
+  element.src = `./assets/${name.toLowerCase()}.jpeg`;
+  element.alt = alt || "이미지 정보가 없습니다";
+}
+
+
+// 이름 변경 함수
+function setNameText(element, name) {
+  element.innerText = name;
+}
+
+
+
+
+// 버튼 클릭 이벤트 적용
 buttons.forEach((button, index) => {
   button.addEventListener("click", () => {
+    const selectedData = data[index];
 
-    const selectedData = data[index]; 
-
-
-    // 바디 배경 색 변경
-    body.style.background = `linear-gradient(to bottom, ${selectedData.color[0]}, ${selectedData.color[1]})`;
     
-
-    // visual 정보 변경
-    visualImg.src = `./assets/${selectedData.name.toLowerCase()}.jpeg`; 
-    visualImg.alt = selectedData.alt; 
-    
-
-    // 이름 변경
-    nickName.innerText = selectedData.name; 
-    
+    // 함수 실행
+    setBgColor(selectedData.color);
+    setImg(visualImg, selectedData.name, selectedData.alt);
+    setNameText(nickName, selectedData.name);
 
 
-    // 우선 버튼의 모든 부모 요소에서 'is-active' 클래스를 지워주고, 
+    // 우선 버튼의 모든 부모 요소에서 'is-active' 클래스를 지워주고,
     // 버튼이 눌리면 버튼의 부모 요소에 'is-active' class를 추가해준다.
     buttons.forEach((button) => {
-      const parent = button.parentElement; 
-      parent.classList.remove("is-active"); 
+      const parent = button.parentElement;
+      parent.classList.remove("is-active");
     });
 
     button.parentElement.classList.add("is-active");
